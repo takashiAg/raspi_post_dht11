0c5b18bcd30879a81c3d58167c0e217c475cf340
安藤亮介 <crawd4274@gmail.com>
2017-11-15 16:30:41 +0900

    5th commit

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ import urllib
 import threading
 
 url = "http://html.takashia.xyz/post_ESP.php"
-Mac_address=mac_address.macaddress()
+Mac_address = mac_address.macaddress()
 
-def post(humi,temp):
+
+def post(humi, temp):
     try:
-        params = {'data': str(humi),'id':Mac_address+"humi"}
+        params = {'data': str(humi), 'id': Mac_address + "humi"}
+        data = urllib.urlencode(params)
+        d = urllib.urlopen(url, data)
+        print(d.data)
+        print("I send a message to " + url + "\nmessage:" + str(humi))
+        params = {'data': str(temp), 'id': Mac_address + "temp"}
         data = urllib.urlencode(params)
         d = urllib.urlopen(url, data)
         print(d)
-        print("I send a message to " + url + "\nmessage:" + humi)
-        params = {'data': str(temp),'id':Mac_address+"temp"}
-        data = urllib.urlencode(params)
-        d = urllib.urlopen(url, data)
-        print(d)
-        print("I send a message to " + url + "\nmessage:" + temp)
+        print("I send a message to " + url + "\nmessage:" + str(temp))
     except:
         print("I try to send a message but missed it")
         return 0
     return 0
 
+
 def main():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.cleanup()
-    GPIO.setup(6,GPIO.OUT)
+    GPIO.setup(6, GPIO.OUT)
     GPIO.output(6, True)
 
     # read data using pin 14
@@ -40,9 +42,10 @@ def main():
         if result.is_valid():
             print("Temperature: %d C" % result.temperature)
             print("Humidity: %d %%" % result.humidity)
-            threading.Thread(target=post, args=(result.humidity,result.temperature,)).start()
+            threading.Thread(target=post, args=(result.humidity, result.temperature,)).start()
             time.sleep(20)
         time.sleep(1)
 
-if __name__=="__main__":
-        main()
+
+if __name__ == "__main__":
+    main()
